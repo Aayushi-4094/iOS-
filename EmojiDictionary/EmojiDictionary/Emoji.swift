d2ac6e63f233ca08f47475544d4ad30978e62f5f
Aayushi Agarwal <97082852+Aayushi-4094@users.noreply.github.com>
2024-03-06 19:32:46 +0530

    Added Emoji Dictionary

---
+++ EmojiDictionary/EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  Emoji Dictionary
+//
+//  Created by user2 on 23/01/24.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
