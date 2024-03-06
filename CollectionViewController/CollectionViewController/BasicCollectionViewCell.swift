504af2f71b5585dd2e24ba3b4606589f5984f3e8
Aayushi Agarwal <97082852+Aayushi-4094@users.noreply.github.com>
2024-03-06 19:30:56 +0530

    Added collection View Controller

---
+++ CollectionViewController/CollectionViewController/BasicCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BasicCollectionViewCell.swift
+//  CollectionViewController
+//
+//  Created by user2 on 29/01/24.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    
+}
